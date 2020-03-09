916fe77dc43eb57af80254a2260eef3f1400df31
kafi62 <kafiraj.27@gmail.com>
2020-03-09 13:15:18 +0600

    2nd Commit

--- app/src/main/java/com/example/airtel/ProfileActivity.java
+++ app/src/main/java/com/example/airtel/ProfileActivity.java
@@ -1,15 +1,23 @@
 package com.example.airtel;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class ProfileActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     private Toolbar toolbar;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,15 @@ public class ProfileActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolBarId);
         setSupportActionBar(toolbar);
 
+        drawerLayout = findViewById(R.id.drawerLayId);
+
+        navigationView = findViewById(R.id.navViewId);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.getDrawerArrowDrawable().setColor(Color.WHITE);
+        actionBarDrawerToggle.syncState();
 
     }
 
